0089d4d2157146fd262a70ce460bf349a8dbe20c
Simon Teame <simonteame436@gmail.com>
2025-09-25 13:25:46 +0200

    Connection tested and it works

--- Auto_Auction/DB/Database.cs
+++ Auto_Auction/DB/Database.cs
@@ -1,6 +1,38 @@
-namespace Auto_Auction.DB;
+using Microsoft.Data.SqlClient;
 
-public class Database
+using System;
+using Microsoft.Data.SqlClient;
+
+namespace Auto_Auction.DB
 {
-    
+    public class Database
+    {
+        private static SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "sql.itcn.dk",
+                InitialCatalog = "mads64g6.SKOLE",
+                UserID = "mads64g6.SKOLE",
+                Password = "Bq6e22EaU3",
+                TrustServerCertificate = true
+            };
+
+            return new SqlConnection(builder.ToString());
+        }
+        
+        public static void TestConnection()
+        {
+            try
+            {
+                using var connection = GetConnection();
+                connection.Open();
+                Console.WriteLine("Connected!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" Failed: {ex.Message}");
+            }
+        }
+    }
 }
--- Auto_Auction/Program.cs
+++ Auto_Auction/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Avalonia;
 using System;
-
+using Auto_Auction.DB;
 namespace Auto_Auction;
 
 sealed class Program
@@ -9,8 +9,13 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
+    public static void Main(string[] args)
+    {
+        Database.TestConnection();
+        BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
+        
+    } 
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
