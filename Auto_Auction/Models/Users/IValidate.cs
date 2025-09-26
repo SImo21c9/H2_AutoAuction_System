d00b7cfca6fb98fa21f6a9a2144f4fb685a885cb
Simon Teame <simonteame436@gmail.com>
2025-09-26 12:32:15 +0200

    Added an interface for User. It's used to validate username and password.

---
+++ Auto_Auction/Models/Users/IValidate.cs
@@ -0,0 +1,6 @@
+namespace Auto_Auction.Models.Users;
+
+public interface IValidate
+{
+    void Validate();
+}
--- Auto_Auction/Models/Users/User.cs
+++ Auto_Auction/Models/Users/User.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace Auto_Auction.Models.Users;
 
-public class User
+public abstract class User : IValidate
 {
     public int Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
+
+    public bool IsCorp => this is CorporateUser;
     public int PostalCode { get; set; }
 
     public User(int id, string userName, string password, int postalCode )
@@ -14,10 +19,21 @@ public class User
         Password = password;
         PostalCode = postalCode;
     }
+
+    public void Validate()
+    {
+        if (!Regex.IsMatch(UserName, @"^[A-Za-Z0-9,-]{3,32}$"))
+            throw new ArgumentException("Invalid UserName, Username must be between 3 and 32 characters");
+        
+        if (!Regex.IsMatch(Password, @"^(?=.[a-z])(?=.[A-Z])(?=.*\d){8,}$"))
+            throw new ArgumentException("Invalid Password, Password must be between 8 and 32 characters");
+    }
     
     
     public override string ToString()
     {
-        return $"Id: {Id}, UserName: {UserName}, Password: {Password}, PostalCode: {PostalCode}";
+      
+        return $" Id: {Id}, UserName: {UserName}, PostalCode: {PostalCode}";
     }
+    
 }
