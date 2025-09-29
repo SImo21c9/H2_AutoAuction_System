8465898c0fffa44b73e8483a6e9d2972ab9925fa
art <art@art.dk>
2025-09-29 12:13:32 +0200

    G1 Fixes ?

---
+++ Auto_Auction/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Auto_Auction.ViewModels;
+
+public partial class LoginWindowViewModel : ViewModelBase
+{
+    // Minimal ViewModel used for design-time and runtime binding in LoginWindow.axaml
+}
