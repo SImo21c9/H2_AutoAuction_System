23158d2a6bc1de777e9853109f822b3b1d07fe0b
art <art@art.dk>
2025-09-30 10:15:10 +0200

    g1 added messagebox, validation and swiching to a new window on login

--- Auto_Auction/Views/MainWindow.axaml.cs
+++ Auto_Auction/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Auto_Auction.Controls;
 using Avalonia.Controls;
 
 namespace Auto_Auction.Views;
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MainContent.Content = new LoginView(this); // Pass reference of MainWindow
     }
 }
---
+++ H2_AutoAuction_System/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace H2_AutoAuction_System;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
