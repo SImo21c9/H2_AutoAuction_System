7e2a0fa3021f1a4e5b1d405851bf951f4cef0fa1
Simon <Simonteame436@gmail.com>
2025-10-06 23:39:04 +0200

    small change

--- Auto_Auction/ViewModels/MainWindowViewModel.cs
+++ Auto_Auction/ViewModels/MainWindowViewModel.cs
@@ -15,12 +15,43 @@ public partial class MainWindowViewModel : ViewModelBase
         CurrentView = new LoginWindowViewModel(this);
     }
     
-    public void NavigateToLogin() => CurrentView = new LoginWindowViewModel(this);
-    public void NavigateToDashboard() => CurrentView = new DashBoardViewModel(this);
-    public void NavigateToCreateUser() => CurrentView = new CreateUserViewModel(this);
-    public void NavigateToSetForSale() => CurrentView = new SetForSaleViewModel(this);
-    public void NavigateToBuyerAuction() => CurrentView = new BuyerofAuctionViewModel(this);
-    public void NavigateToBidHistory() => CurrentView = new BidHistoryViewModel(this);
-    public void NavigateToMakeBid() => CurrentView = new MakeABidViewModel(this);
-    public void NavigateToProfile() => CurrentView = new UserProfileViewModel(this);
+    public void NavigateToLogin()
+    {
+        CurrentView = new LoginWindowViewModel(this);
+    }
+
+    public void NavigateToDashboard()
+    {
+        CurrentView = new DashBoardViewModel(this);
+    }
+
+    public void NavigateToCreateUser()
+    {
+        CurrentView = new CreateUserViewModel(this);  
+    }
+
+    public void NavigateToSetForSale()
+    {
+        CurrentView = new SetForSaleViewModel(this);
+    }
+
+    public void NavigateToBuyerAuction()
+    {
+        CurrentView = new BuyerofAuctionViewModel(this);
+    }
+
+    public void NavigateToBidHistory()
+    {
+        CurrentView = new BidHistoryViewModel(this);
+    }
+
+    public void NavigateToMakeBid()
+    {
+        CurrentView = new MakeABidViewModel(this);
+    }
+
+    public void NavigateToProfile()
+    {
+        CurrentView = new UserProfileViewModel(this);
+    }
 }
